747f64f22f755f5fb2ca5e0217f967695891f0c2
Unknown <luke@kaal.im>
2017-03-02 17:48:43 +1100

    Refactoring tests
    Adding a couple tests for bad input cases, and reorganised the way I hadle them

--- test/main.js
+++ test/main.js
@@ -1,3 +1,9 @@
+
+/**
+    These are the Mocha intergration tests. They are invoked by 'npm test', or
+    /bin/test.js.
+ */
+
 const assert = require('assert');
 const request = require('supertest');
 
@@ -7,20 +13,21 @@ const expected400ErrorMessage = {
 }
 
 //Utility function, to quickly test the JSON parsed body of a response
-//to a predetermined object.
+//to a predetermined object. If parsing the response fails, then we
+//throw an error anyway, which fails the test, so no try/catch block
+//needed/
 function MatchErrorMessage(body, expectedError) {
-    console.log(body);
     responseBody = JSON.parse(body);
-    assert.deepEqual('responseBody', expectedError, "Did not recieve the expected error message");
+    assert.deepEqual(responseBody, expectedError, "Did not recieve the expected error message");
 }
 
 //This function will be called by bin/test.js, where server is a http server
 module.exports = function TestRoutes(server) {
 
   //these tests will send mock requests to an instance of the app
-  describe('Routes', function() {
+  describe('Server', function() {
     //tests for requests aimed at the root url for the domain.
-    describe('root' , function() {
+    describe('/' , function() {
 
       //Test#1
       it('should return error 400 and a specific error message when the body is empty', function(done) {
@@ -32,37 +39,56 @@ module.exports = function TestRoutes(server) {
           .expect(function(res) {
             MatchErrorMessage(res.text, expected400ErrorMessage);
           })
-          .end(function(err) {
-            if (err)
-              return done(err);
-            done();
-          })
+          .end(done)
       });
 
       //Test#2
       it('should return error 400 and a specific error when the body does not contain valid JSON', function(done) {
+        const invalidJSON = "this is not valid JSON."
+
         request(server)
           .post('/')
-          .type('text')
-          .send("This is not valid JSON. At least, I would hope so.")
+          .type('application/json') //This is a total lie.
+          .send(invalidJSON)
           .expect(400)
           .expect(function(res) {
             MatchErrorMessage(res.text, expected400ErrorMessage);
           })
-          .end(function(err) {
-            if (err)
-              return done(err);
-            done();
+          .end(done)
+      });
+
+      //Test#3
+      it('should return error 400 and a specific error when there is no payload property', function(done) {
+        const invalidPayload = {
+          notPayload: [
+            { title: 'not-a-real-show', 'drm' : true, 'episodeCount' : 1}
+          ]
+        };
+
+        request(server)
+          .post('/')
+          .type('application/json')
+          .send(invalidPayload)
+          .expect(400)
+          .expect(function(res) {
+            MatchErrorMessage(res.text, expected400ErrorMessage);
           })
+          .end(done)
       });
 
     });
     //these test describe requests aimed for non defined routes
     describe('other', function() {
+      //Test#4
       it('should return 404 when the request is an invalid url', function(done) {
+        const fakeURL = "/not-a-real-url"; //This URL should be invalid.
+        //TODO: how do I figure out which urls aren't real? should I just
+        //input like 20 random chars? I think that's a different type of test
+
         request(server)
-          .post('/not-a-real-url')
-          .expect(404, done);
+          .post(fakeURL)
+          .expect(404)
+          .end(done)
       });
     });
   });
